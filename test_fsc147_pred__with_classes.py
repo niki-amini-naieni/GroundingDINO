11832aa5be2d99b63e9cd3b2bdbca163050ece04
Niki Amini-Naieni <niki@adainnovations.ai>
2024-01-22 17:23:23 +0000

    use singularized/correct class names

--- test_fsc147_pred__with_classes.py
+++ test_fsc147_pred__with_classes.py
@@ -11,7 +11,7 @@ CONFIG_PATH = "./groundingdino/config/GroundingDINO_SwinB_cfg.py"
 WEIGHTS_PATH = "../weights/groundingdino_swinb_cogcoor.pth"
 DATA_SPLIT_PATH = "/scratch/shared/beegfs/nikian/FSC-147/Train_Test_Val_FSC_147.json"
 IMG_DIR = "/scratch/shared/beegfs/nikian/FSC-147/images_384_VarV2"
-CLASS_NAME_PATH = "/scratch/shared/beegfs/nikian/FSC-147/ImageClasses_FSC147.txt"
+CLASS_NAME_PATH = "correct_fsc147_class_names.txt"
 FSC147_ANNO_FILE = "/scratch/shared/beegfs/nikian/FSC-147/annotation_FSC147_384.json"
 FSC147_D_ANNO_FILE = "../CounTX-plusplus/FSC-147-D.json"
 DATA_SPLIT = "val"
@@ -21,9 +21,6 @@ with open(DATA_SPLIT_PATH) as f:
     data_split = json.load(f)
 image_names = data_split[DATA_SPLIT]
 
-with open(FSC147_D_ANNO_FILE) as f:
-    fsc147_d_annotations = json.load(f)
-
 with open(FSC147_ANNO_FILE) as f:
     fsc147_annotations = json.load(f)
 
@@ -31,16 +28,12 @@ pluralizer = Pluralizer()
 
 class_dict = {}
 
-if descriptions == "fsc147":
-  with open(CLASS_NAME_PATH) as f:
-      for line in f:
-          key = line.split()[0]
-          val = line.split()[1:]
-          class_dict[key] = pluralizer.singular(' '.join(val))
-          #class_dict[key] = ' '.join(val)
-else:
-   for img_name in image_names:
-      class_dict[img_name] = pluralizer.singular(fsc147_d_annotations[img_name]["text_description"][4:])
+with open(CLASS_NAME_PATH) as f:
+    for line in f:
+        key = line.split()[0]
+        val = line.split()[1:]
+        class_dict[key] = pluralizer.singular(' '.join(val))
+        #class_dict[key] = ' '.join(val)
 
 classes = list(np.unique(list(class_dict.values())))
 
